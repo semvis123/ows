c68cbdab9a6da73c357a41ee2dc97a892af52cda
semvis123 <semvisscher10@gmail.com>
2024-02-02 18:50:12 +0100

    add ant test scaffolding

---
+++ game.ant.test.php
@@ -0,0 +1,17 @@
+<?php
+
+include_once 'test.php';
+include_once 'game.php';
+include_once 'database.php';
+
+describe("Soldier ant implementation", function () {
+    describe("A soldier ant moves by sliding an unlimited number of times", function () {
+        // TODO: Add tests
+    });
+    describe("A soldier cannot move through filled spaces", function () {
+        // TODO: Add tests
+    });
+    describe("A soldier ant must move at least one space", function () {
+        // TODO: Add tests
+    });
+});
